fe99a674b4ff6621615f5af3640838c96935178c
brettmccausland <brettmccausland@yahoo.com>
2018-03-30 12:07:59 -0700

    h

--- action.js
+++ action.js
@@ -1,3 +1,10 @@
 $('.carousel').carousel({
   interval: 1100
 })
+
+var Contact_btn = document.getElementById("submit-btn");
+Contact_btn.addEventlistner("click",function(){
+	var Clientemail = document.getElementById("usremail");
+	var ClientMessage = document.getElementById("message");
+	
+}); 
