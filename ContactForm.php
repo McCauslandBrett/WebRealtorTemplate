f4a02c6822f94f85bfb046a912feef7a3e9e1a90
brettmccausland <brettmccausland@yahoo.com>
2018-06-23 17:36:01 -0700

    save

--- ContactForm.php
+++ ContactForm.php
@@ -1,13 +1,13 @@
 <?php 
-	$name = $_POST['name'];
+	#$name = $_POST['name'];
 	$visitor_email= $_POST['usremail'];
 	$message = $_POST['message'];
 
-	$email_from = 'alondraWebPage@adress.com';
+	$email_from = 'brettmccausland@gmail@gmail.com';
 	$email_subject = "new form submission";
-	$email_body = "User Name: $name.\n"."User Email: $visitor_email.\n"."User Message: $message.\n";
+	$email_body = "User Email: $visitor_email.\n"."User Message: $message.\n";
 
-   $to = "alondramccausland@gmail.com";
+   $to = "brettmccausland@gmail.com";
    $headers = "From: $email_from \r\n";
    $headers .= "Reply-To: $visitor_email \r\n";
    mail($to, $email_subject, $email_body,$headers);
---
+++ server.js
@@ -0,0 +1,3 @@
+const express=require("express");
+let app = express();
+app.set("view engine","pug");
